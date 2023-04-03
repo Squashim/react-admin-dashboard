eedaa900b8d438b44b09f90c810bf04be42de5f9
Squashim <konrad18v1@wp.pl>
2023-04-03 19:25:22 +0200

    Added Charts page and dashboard

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,5 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
 const Line = () => {
-	<div>Line</div>;
+	return (
+		<Box m='20px'>
+			<Header title='Line Chart' subtitle='Simple Line Chart'></Header>
+			<Box height='75vh'>
+				<LineChart />
+			</Box>
+		</Box>
+	);
 };
 
 export default Line;
